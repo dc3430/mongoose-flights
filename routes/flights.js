10c06bdb596d87980c611f7f7955b746302f502a
Daniel <cermak222@gmail.com>
2019-08-15 20:16:50 -0500

    add flightCtrl.new

---
+++ routes/flights.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var flightsCtrl = require('../controllers.flights');
+
+/* GET /flights/new */
+router.get('/new',flightsCtrl.new);
+
+module.exports = router;
