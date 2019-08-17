42ab1ccd04ceb7d41323b53bb8fd026e021a746c
Daniel <cermak222@gmail.com>
2019-08-17 13:30:22 -0500

    add create all flight

--- controllers/flights.js
+++ controllers/flights.js
@@ -6,9 +6,13 @@ module.exports = {
     create, 
     index,
     show,
-    // addDestination,
+    addDestination,
     // deleteFlight,
     // addTicket
+}
+
+function addDestination(req, res) {
+    Flight.findById(req)
 }
 
 function show(req, res) {
@@ -25,16 +29,35 @@ function index(req, res) {
     })
 }
 
+// function create(req, res) {
+//     for(let key in req.body) {
+//         if(req.body[key] === '') delete req.body[key];
+//     }
+//     var flight = new Flight(req.body);
+//     flight.save(req.body, function(err, flight) {
+//       // one way to handle errors
+//     //  if (err) return res.redirect('flights/new');
+//     console.log(err);
+//       console.log(flight);
+//       // for now, redirect right back to new.ejs
+//       res.redirect('/flights');
+//     });
+// }
+
 function create(req, res) {
-    var flight = new Flight(req.body);
-    flight.save(function(err) {
-      // one way to handle errors
-      //if (err) return res.render('flights/new');
-      console.log(flight);
-      // for now, redirect right back to new.ejs
-      res.redirect('/flights');
+    for(let key in req.body){
+        req.body[key] === '' && delete req.body[key];
+    }
+    Flight.create(req.body, function(err, flight) {
+        console.log(flight);
+        res.redirect('/flights');
     });
-  }
+}
+
+
+
+ 
+
 
 function newFlights(req, res) {
     //open a new form for entering a new flight
